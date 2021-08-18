cc67e48f7e18b12098768ecb431dec4002b2a823
dSandley20 <danielsandley2@gmail.com>
2021-08-18 00:55:13 -0400

    added dice images

--- app/src/main/java/com/example/happybirthdayapp/models/Dice.kt
+++ app/src/main/java/com/example/happybirthdayapp/models/Dice.kt
@@ -3,7 +3,7 @@ package com.example.happybirthdayapp.models
 class Dice (private val numSides: Int) {
     private val sides : IntRange = 1..numSides
 
-    fun rollDice (): String {
-        return this.sides.random().toString()
+    fun rollDice (): Int {
+        return this.sides.random()
     }
 }

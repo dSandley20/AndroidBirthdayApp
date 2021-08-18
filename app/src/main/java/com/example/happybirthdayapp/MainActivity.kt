4bb1b4737bd8adcedae397ebddc67e131aa29fa4
dSandley20 <danielsandley2@gmail.com>
2021-08-17 23:18:34 -0400

    reset view and add button w/ function to open text

--- app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.happybirthdayapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //gets the button and assigns it the variable rollButton
+        //still have to do it by the ID assigned in the design window, just happens that its also connected to a int that AS handles.
+        val rollButton: Button = findViewById(R.id.Roll_Dice_Button)
+        //this serves as a function but without the ()
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+
+
     }
 }

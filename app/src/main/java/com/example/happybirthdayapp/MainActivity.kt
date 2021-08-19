8719554c4090717a78d9672503c3224fce61ad4d
dSandley20 <danielsandley2@gmail.com>
2021-08-19 01:04:36 -0400

    Load and display affirmations in the RecyclerView

--- app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.happybirthdayapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.happybirthdayapp.models.adapter.ItemAdapter
+import com.example.happybirthdayapp.models.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val affirmationDataSet = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, affirmationDataSet)
+        recyclerView.setHasFixedSize(true)
     }
 }

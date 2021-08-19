d724d7c0374d67cf2cf0b86c0c941858d515c134
dSandley20 <danielsandley2@gmail.com>
2021-08-19 01:01:37 -0400

    Added comments for future refrence

--- app/src/main/java/com/example/happybirthdayapp/models/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/happybirthdayapp/models/adapter/ItemAdapter.kt
@@ -14,21 +14,34 @@ class ItemAdapter(
     private val dataset: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
     }
 
+    /**
+     * Create new views (invoked by the layout manager)
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
        val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
 
     }
 
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
     override fun getItemCount(): Int {
         return dataset.size
     }

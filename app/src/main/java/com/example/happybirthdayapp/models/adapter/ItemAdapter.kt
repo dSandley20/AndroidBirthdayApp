4a6b9c19631bd0d1fdb438134deaa2c1357de469
dSandley20 <danielsandley2@gmail.com>
2021-08-19 00:56:21 -0400

    set up initial ItemAdapter and ItemViewHolder

--- app/src/main/java/com/example/happybirthdayapp/models/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/happybirthdayapp/models/adapter/ItemAdapter.kt
@@ -1,4 +1,14 @@
 package com.example.happybirthdayapp.models.adapter
 
-class ItemAdapter {
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.happybirthdayapp.R
+import com.example.happybirthdayapp.models.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
 }

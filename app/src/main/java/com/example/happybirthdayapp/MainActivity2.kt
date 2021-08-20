3b09541c308e7321519e1ea3f4412ad5a102e9c4
dSandley20 <danielsandley2@gmail.com>
2021-08-19 23:23:57 -0400

    learned how to switch between multiple activities

--- app/src/main/java/com/example/happybirthdayapp/MainActivity2.kt
+++ app/src/main/java/com/example/happybirthdayapp/MainActivity2.kt
@@ -3,22 +3,21 @@ package com.example.happybirthdayapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.happybirthdayapp.databinding.ActivityMainBinding
+import com.example.happybirthdayapp.databinding.ActivityMain2Binding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
-    private  lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.switchActivity1.setOnClickListener{
-            val intent = Intent(this, MainActivity2::class.java)
+        binding.switchActivity2.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
-
     }
 }

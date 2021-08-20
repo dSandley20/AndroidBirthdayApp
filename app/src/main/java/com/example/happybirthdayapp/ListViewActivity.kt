5d9568203f1d6e8999b46ad56a64cfe6192530bf
dSandley20 <danielsandley2@gmail.com>
2021-08-20 01:16:31 -0400

    added logic to display/populate the list

--- app/src/main/java/com/example/happybirthdayapp/ListViewActivity.kt
+++ app/src/main/java/com/example/happybirthdayapp/ListViewActivity.kt
@@ -2,6 +2,9 @@ package com.example.happybirthdayapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.happybirthdayapp.adapters.ItemAdapter
+import com.example.happybirthdayapp.data.Datasource
 import com.example.happybirthdayapp.databinding.ActivityListViewBinding
 
 class ListViewActivity : AppCompatActivity() {
@@ -11,5 +14,9 @@ class ListViewActivity : AppCompatActivity() {
         binding = ActivityListViewBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val affirmationDataSet = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, affirmationDataSet)
+        recyclerView.setHasFixedSize(true)
     }
 }

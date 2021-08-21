400e967705a8521c6af7436af8bd0277e12fb7a7
dSandley20 <danielsandley2@gmail.com>
2021-08-21 18:55:04 -0400

    added volley, AuthActivity to android manifest

--- app/src/main/java/com/example/happybirthdayapp/AuthActivity.kt
+++ app/src/main/java/com/example/happybirthdayapp/AuthActivity.kt
@@ -1,4 +1,8 @@
 package com.example.happybirthdayapp
 
-class AuthActivity {
+import androidx.appcompat.app.AppCompatActivity
+import com.example.happybirthdayapp.databinding.ActivityAuthBinding
+
+class AuthActivity: AppCompatActivity() {
+    private lateinit var binding : ActivityAuthBinding
 }

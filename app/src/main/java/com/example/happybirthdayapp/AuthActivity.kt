4a6bb34af6296c981b2a29eea3dec2f891bdc5f8
dSandley20 <danielsandley2@gmail.com>
2021-08-21 18:57:03 -0400

    finished adding starting logic to auth activity

--- app/src/main/java/com/example/happybirthdayapp/AuthActivity.kt
+++ app/src/main/java/com/example/happybirthdayapp/AuthActivity.kt
@@ -1,8 +1,16 @@
 package com.example.happybirthdayapp
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.happybirthdayapp.databinding.ActivityAuthBinding
+import com.example.happybirthdayapp.databinding.ActivityBirthdayBinding
 
 class AuthActivity: AppCompatActivity() {
     private lateinit var binding : ActivityAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
 }
